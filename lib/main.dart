8339078e1f5b4502287fbc341c8c3183281966c9
kkan0615 <kkan0615@gmail.com>
2022-01-04 22:18:56 +0900

    :rocket: first commit

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -49,6 +49,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  int _selectedIndex = 1;
 
   void _incrementCounter() {
     setState(() {
@@ -58,6 +59,12 @@ class _MyHomePageState extends State<MyHomePage> {
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
+    });
+  }
+
+  void _onTapNavigatorBarItem(int index) {
+    setState(() {
+      _selectedIndex = index;
     });
   }
 
@@ -102,6 +109,13 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
+            Text(
+              'Selected index: ',
+            ),
+            Text(
+              '$_selectedIndex',
+              style: Theme.of(context).textTheme.headline4,
+            ),
           ],
         ),
       ),
@@ -110,6 +124,25 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _selectedIndex, //현재 선택된 Index
+        onTap: _onTapNavigatorBarItem,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            title: Text('Favorites'),
+            icon: Icon(Icons.favorite),
+          ),
+          BottomNavigationBarItem(
+            title: Text('Music'),
+            icon: Icon(Icons.music_note),
+          ),
+          BottomNavigationBarItem(
+            title: Text('Places'),
+            icon: Icon(Icons.location_on),
+          ),
+        ],
+      ),
     );
   }
 }
